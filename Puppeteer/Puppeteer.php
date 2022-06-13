ac7fee2280b377a1355a1eeefeea9070fafdccc2
Nick Blackwell <nickblackwell82@gmail.com>
2022-06-12 20:51:23 -0700

    add database table

--- Puppeteer/Puppeteer.php
+++ Puppeteer/Puppeteer.php
@@ -8,6 +8,7 @@ class Puppeteer extends \core\extensions\Plugin implements
 	\core\DatabaseProvider{
 
 	use \core\extensions\widget\WidgetProviderTrait;
+	use \core\DatabaseProviderTrait;
 
 	protected $description = 'Manages Puppeteer Tests and Scripts via API calls';
 
