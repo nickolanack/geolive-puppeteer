50639b7d05682096f27cdb501f0214dbcf17850d
Nick Blackwell <nickblackwell82@gmail.com>
2022-06-13 19:07:56 -0700

    create output folder, add out arg, and use url hash

--- Puppeteer/Widgets/PuppeteerScript/PuppeteerScriptWidget.php
+++ Puppeteer/Widgets/PuppeteerScript/PuppeteerScriptWidget.php
@@ -9,10 +9,21 @@ class PuppeteerScriptWidget extends \core\extensions\Widget{
 	public function runScript($args){
 
 
+		$outdir=GetPath("{front}/../puppeteer/{domain}/");
+		if(!file_exists($outdir)){
+			if(!mkdir($outdir, 0700, true){
+				throw new \Exception('Failed to create: '.$outdir);
+			}
+		}
+
+
 		$dir=getcwd();
 		chdir(__DIR__);
 
-		echo shell_exec('node test.js '.escapeshellarg(json_encode(array("url"=>$args->url))));
+		echo shell_exec('node test.js '.escapeshellarg(json_encode(array(
+			"url"=>$args->url,
+			"out"=>$outdir.'/'.md5($args->url).'.png'
+		))));
 
 		chdir($dir);
 	}
