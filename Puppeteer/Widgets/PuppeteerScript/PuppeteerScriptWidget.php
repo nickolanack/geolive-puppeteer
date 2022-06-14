fcee201e85d53065e928b905d5ffd096c2929ce8
Nick Blackwell <nickblackwell82@gmail.com>
2022-06-13 17:21:07 -0700

    add test code

--- Puppeteer/Widgets/PuppeteerScript/PuppeteerScriptWidget.php
+++ Puppeteer/Widgets/PuppeteerScript/PuppeteerScriptWidget.php
@@ -9,10 +9,12 @@ class PuppeteerScriptWidget extends \core\extensions\Widget{
 	public function runScript($args){
 
 
+		$dir=getcwd();
+		chdir(__DIR__);
 
-		
+		echo shell_exec('node test.js '.escapeshellarg(json_encode(array("url"=>$args->url)));
 
-		
+		chdir($dir);
 	}
 	
 }
